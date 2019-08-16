53b2e67113736dcda0b17a8dd7a69edb70bc345a
boxplay <903199865@qq.com>
2019-08-16 14:14:25 +0800

    更换链接

--- src/mob/components/Mob.vue
+++ src/mob/components/Mob.vue
@@ -292,22 +292,8 @@
 				}
 			},
 			openInWebview () {
-				var ua = navigator.userAgent.toLowerCase()
-				if (ua.match(/MicroMessenger/i) == 'micromessenger') { // 微信浏览器判断
-					return false
-				} else if (ua.match(/QQ/i) == 'qq') { // QQ浏览器判断
-					return false
-				} else if (ua.match(/WeiBo/i) == "weibo") {
-					return false
-				} else {
-					if (ua.match(/Android/i) != null) {
-						return ua.match(/browser/i) == null
-					} else if (ua.match(/iPhone/i) != null) {
-						return ua.match(/safari/i) == null
-					} else {
-						return (ua.match(/macintosh/i) == null && ua.match(/windows/i) == null)
-					}
-				}
+				var userAgent = navigator.userAgent.toLowerCase();
+				return userAgent.match(/videoarticle\/([\d.]+)/)
 			},
 			scrollEvent(type){//导航栏滚动事件
 				if(this._isMobile()){
